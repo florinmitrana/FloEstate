c1eaaed8d5ee15ce2ef9a545d28c337776ef301d
florinmitrana <f.mitrana@yahoo.com>
2024-08-06 09:58:31 +0300

    user argument bug solved- fetching data based on URL query done

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -1,21 +1,38 @@
 import prisma from "../lib/prisma.js";
 
-export const getPosts = async (req,res) =>{
-    try{
-        const posts = await prisma.post.findMany()
+export const getPosts = async (req, res) => {
+    const query = req.query;
+    try {
+        const posts = await prisma.post.findMany({
+            where: {
+                city: query.city || undefined,
+                type: query.type || undefined,
+                property: query.property|| undefined,
+                property: query.property|| undefined,
+                bedroom: parseInt(query.bedroom) || undefined,
+                price: {
+                    gte: parseInt(query.minPrice) || 0,
+                    lte: parseInt(query.maxPrice) || 1000000,
+                }
+
+
+
+
+            }
+        })
         res.status(200).json(posts)
-    }catch(err) {console.log(err); res.status(500).json({message: "Failed to get posts"})}
+    } catch (err) { console.log(err); res.status(500).json({ message: "Failed to get posts" }) }
 }
-export const getPost = async (req,res) =>{
+export const getPost = async (req, res) => {
     const id = req.params.id;
-    try{
+    try {
         const post = await prisma.post.findUnique({
-            where: {id},
-            include:{
+            where: { id },
+            include: {
                 postDetail: true,
                 user: {
-                    select:{
-                        username:true,
+                    select: {
+                        username: true,
                         avatar: true,
                     },
                 },
@@ -23,50 +40,49 @@ export const getPost = async (req,res) =>{
         });
 
         res.status(200).json(post)
-    }catch(err) {console.log(err); res.status(500).json({message: "Failed to get post"})}
+    } catch (err) { console.log(err); res.status(500).json({ message: "Failed to get post" }) }
 }
-export const addPost = async (req,res) =>{
+export const addPost = async (req, res) => {
     const body = req.body;
     const tokenUserId = req.userId;
-    console.log(tokenUserId);
-    try{
+    try {
         const newPost = await prisma.post.create({
-            data:{
+            data: {
                 ...body.postData,
                 userId: tokenUserId,
-                postDetail:{
-                    create:body.postDetail,
-                }
+                postDetail: {
+                    create: body.postDetail,
+                },
             },
         });
         if (!req.userId) {
             return res.status(401).json({ message: "Unauthorized" });
-          }
+        }
         res.status(200).json(newPost)
-    }catch(err) {console.log(err); res.status(500).json({message: "Failed add posts"})}
+    } catch (err) { console.log(err); res.status(500).json({ message: "Failed add posts" }) }
 }
-export const updatePost = async (req,res) =>{
-    try{
+export const updatePost = async (req, res) => {
+    try {
         res.status(200).json()
-    }catch(err) {console.log(err); res.status(500).json({message: "Failed update posts"})}
+    } catch (err) { console.log(err); res.status(500).json({ message: "Failed update posts" }) }
 }
 
-export const deletePost = async (req,res) =>{
+export const deletePost = async (req, res) => {
     const id = req.params.id
     const tokenUserId = req.userId
-    try{
+    try {
         const post = await prisma.post.findUnique({
-            where:{id},
+            where: { id },
         });
 
-        if(post.userId != tokenUserId){
-            return res.status(403).json({message: 'Not Authorized!'})
+        if (post.userId != tokenUserId) {
+            return res.status(403).json({ message: 'Not Authorized!' })
         }
         await prisma.post.delete({
-            where: {id},
+            where: { id },
         });
 
 
-        res.status(200).json({message: "post deleted"})
-    }catch(err) {console.log(err); res.status(500).json({message: "Failed delete posts"})}
+        res.status(200).json({ message: "post deleted" })
+    } catch (err) { console.log(err); res.status(500).json({ message: "Failed delete posts" }) }
 }
