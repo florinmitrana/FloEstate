e65414446c41836553a83efc55a497c0ae290735
florinmitrana <f.mitrana@yahoo.com>
2024-08-06 12:11:32 +0300

    fetch profile post list CRUD done

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,14 +1,15 @@
 import express from "express";
 import {login, logout, register} from "../api/controllers/auth.controller.js"
-import { deleteUser, getUser, getUsers, savePost, updateUser } from "../api/controllers/user.controller.js";
+import { deleteUser, getUser, getUsers, savePost, updateUser,profilePosts } from "../api/controllers/user.controller.js";
 import {verifyToken} from "../api/middleware/verifyToken.js"
 const router  = express.Router();
 
 router.get("/", getUsers);
-router.get("/:id", verifyToken, getUser);
+//router.get("/:id", verifyToken, getUser);
 router.put("/:id", verifyToken, updateUser);
 router.delete("/:id",verifyToken,deleteUser);
 router.post("/save", verifyToken, savePost);
+router.get("/profilePosts",verifyToken,profilePosts);
 
 
 export default router;
