ac9f61e088b5dfa223d1e21a6518b35b1874db52
florinmitrana <f.mitrana@yahoo.com>
2024-08-06 15:20:24 +0300

    chat api bug solved

--- routes/message.route.js
+++ routes/message.route.js
@@ -3,7 +3,7 @@ import { addMessage} from "../api/controllers/message.controller.js";
 import {verifyToken} from "../api/middleware/verifyToken.js"
 const router  = express.Router();
 
-router.get("/:chatId",verifyToken,addMessage);
+router.post("/:chatId",verifyToken,addMessage);
 
 
 export default router;
